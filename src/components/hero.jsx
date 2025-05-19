64f38fc2a89e51db4bbc2991936192e3eb095acf
omekeuchechi <omekejoseph97@gmail>
2025-05-19 08:06:03 +0100

    updated some changes in the front page

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -4,8 +4,8 @@ import { useState } from "react";
 
 const HeroSection = () => {
     return(
-        <header className={"hero-section"}>
-            <div class={"hero-content"}>
+        <header className="hero-section">
+            <div className="hero-content">
                 <h1>Profitable Digital Skills, Tech Solutions and IT Gadgets</h1>
                 <a href="/About" className="btn">Get Started</a>
             </div>
